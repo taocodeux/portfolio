f1d7a333f4ba5e6f515c4f9add61642a89af8a3e
taocodeux <taofeekatb1@gmail.com>
2023-09-07 05:44:33 +0100

    first commit

---
+++ myportfolio.js
@@ -0,0 +1,25 @@
+let changeText = document.getElementById("change-text")
+let wordsArr = ["Web-Developer", "Product-Designer"]
+let currentLetterIndex = 0
+let currentWordIndex = 0
+let typingSpeed = 100
+function typeLetters(){
+    if (currentLetterIndex < wordsArr[currentWordIndex].length){
+        changeText.innerText += wordsArr[currentWordIndex][currentLetterIndex]
+        currentLetterIndex++
+        setTimeout(typeLetters, typingSpeed)
+    }else{
+        setTimeout(clearLetters,typingSpeed)
+    }
+}
+function clearLetters(){
+    if(changeText.innerText){
+        changeText.innerText = changeText.innerText.slice(0, -1)
+        setTimeout(clearLetters, typingSpeed)
+    }else{
+        currentWordIndex = (currentWordIndex + 1) % wordsArr.length
+        currentLetterIndex = 0
+        setTimeout(typeLetters, typingSpeed)
+    }
+}
+typeLetters()
